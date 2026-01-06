4a948d861f04feb9026d5c5e1141d7719f180be2
meokurocute2 <meokurocute2@gmail.com>
2026-01-06 15:39:14 +0700

    make homework

---
+++ SS1.py
@@ -0,0 +1,15 @@
+# print(1+1)
+# print("Hello world")
+# print("This is about my life")
+# print("I love programming")
+# print("The time a hard")
+# print('Hey Kevin looks')
+print('1+1') 
+print(1+1)
+print("Hello"+" "+"world")
+
+
+x = input("My name is ?")
+print(f"Hello, {x}")
+
+
---
+++ SS2.py
@@ -0,0 +1,3 @@
+box = "Key", "10", "vest"
+print(f"Inside the box we have {box}")
+
