6ef92c6fb0cd1b6a98993d50f444e7d0fefcb63d
meokurocute2 <meokurocute2@gmail.com>
2026-01-08 14:58:25 +0700

    EX1

---
+++ SS1_HM.py
@@ -0,0 +1,21 @@
+# Question 1
+name = input("What's your name? ")
+print(f"Hello, {name}! Welcome to Python")
+
+#Question 2
+A = int(input("Enter first number: "))
+B = int(input("Enter second number: "))
+print(f"The sum is: {A + B}")
+
+# Question 3
+Color = input("What's your favorite color?: ")
+print(f"Wow, {Color} is a beautiful color!")
+
+#Question 4
+YOB = int(input("What is your Year of Birth?: "))
+age = 2026 - int(YOB)
+print (f"You are {age} years old this year.")
+
+# Question 5
+x = int(input("Input your integers number: "))
+print(f"Your binary number is: {bin(x)}")
--- SS2.py
+++ SS2.py
@@ -1,3 +1,15 @@
-box = "Key", "10", "vest"
-print(f"Inside the box we have {box}")
+# box = "Key", "10", "vest"
+# print(f"Inside the box we have {box}")
 
+# # Question 1
+# x: 30
+# print(x)
+
+# #Question 2
+# name = "Vo Tong"
+# print(name)
+
+# Question 3
+
+false = True
+print(type(false))
