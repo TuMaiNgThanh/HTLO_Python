db67146a14459b06333985ab82a3097f230e1854
meokurocute2 <meokurocute2@gmail.com>
2026-01-21 11:18:35 +0700

    SS6

---
+++ BT_QQ.py
@@ -0,0 +1,11 @@
+# # n=int(input("Enter the number: "))
+# # print(bin(n))
+# #Chào anh tuấn đẹp trai ^^
+# print("Hello my nigga")
+# input("Press Enter to continue...")
+
+# name = input("What's your name? ")
+# print(f"Hello {name}, Welcome to Python")
+
+
+print(type(str(input("Enter the number: "))))
